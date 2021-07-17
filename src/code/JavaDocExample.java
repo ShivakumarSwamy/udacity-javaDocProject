ec642322c3210d8dcc9df849741d5be4b3b5d8a3
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2021-07-17 18:53:00 +0200

    Initial Commit

---
+++ src/code/JavaDocExample.java
@@ -0,0 +1,19 @@
+package code;
+
+/**
+ * This is my first JavaDoc
+ *
+ * @author shiva
+ */
+public class JavaDocExample {
+
+    /**
+     * Return backs the provided string
+     *
+     * @param aValue string needs to be return backed
+     * @return string provided to method
+     */
+    public String returnGiveString(String aValue) {
+        return aValue;
+    }
+}
